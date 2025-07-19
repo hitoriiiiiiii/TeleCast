485c7238bcb1145ce59ccdfab84a797882663087
GeniusPrarthana <Prarthanagade25@gmail.com>
2025-07-19 19:37:21 +0200

    Boiler Plate of TeleCast

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from '@/components/ui/button';
+const Page = () => {
+  return (
+    <div className = "items-center justify-center w-full bg-amber-200 h-full">
+      <h1 className = "text-2xl underline "> Baby girl</h1>
+      <Button>
+        Let's get started Baby!
+      </Button>
+    </div>
+  )
+}
+
+export default Page
---
+++ app/subscription/page.tsx
@@ -0,0 +1,7 @@
+const Subscription = () =>{
+    return(
+        <div>Subscriptions</div>
+    )
+}
+
+export default Subscription;
